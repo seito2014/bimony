5b8da0d882d4398522bac79ac00f5ccd2e14ab03
Seito Horiguchi <horiguchi_seito@liginc.co.jp>
2017-05-14 03:17:38 +0900

    Update index

---
+++ src/pc/js/index.js
@@ -0,0 +1,10 @@
+$('#js-hero-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    responsive: true,
+    prevArrow: false,
+    nextArrow: false
+});
